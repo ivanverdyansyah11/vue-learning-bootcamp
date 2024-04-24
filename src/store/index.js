44cdb7497bced17dab38e04706110e7f4a699e1e
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-04-24 20:29:04 +0800

    Progress: Update index store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore } from "vuex";
 import recipe from "./recipe.js";
+import auth from "./auth.js";
 
 export const store = createStore({
     modules: {
         recipe,
+        auth,
     }
 })
