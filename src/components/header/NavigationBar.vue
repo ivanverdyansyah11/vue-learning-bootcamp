5fd3e901ee7dc5bfffd86e4d4816c40a8805f3b7
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-04-24 20:28:00 +0800

    Progress: Update Navigation Bar

--- src/components/header/NavigationBar.vue
+++ src/components/header/NavigationBar.vue
@@ -1,15 +1,41 @@
-<script setup>
-import SearchMenu from "@/components/header/SearchMenu.vue";
-import SignupMenu from "@/components/header/SignupMenu.vue";
-</script>
-
 <template>
   <div class="header__navbar row justify-content-between align-items-center" style="width: 450px;">
     <SearchMenu/>
-    <SignupMenu/>
+    <component :is="components[menuComponent]"></component>
   </div>
 </template>
 
-<style>
+<script setup>
+import SearchMenu from "@/components/header/SearchMenu.vue";
+import SignupMenu from "@/components/header/SignupMenu.vue";
+import ProfileMenu from "@/components/header/ProfileMenu.vue";
 
-</style>
+import { computed, ref, watch } from "vue";
+import { useStore } from "vuex";
+
+const menuComponent = ref('signup-menu')
+const store = useStore()
+
+const components = {
+  'signup-menu': SignupMenu,
+  'profile-menu': ProfileMenu,
+}
+
+const getToken = computed(() => {
+  return store.state.auth.token
+})
+
+if (!getToken.value) {
+  menuComponent.value = "signup-menu"
+} else {
+  menuComponent.value = "profile-menu"
+}
+
+watch(getToken, (newValue, oldValue) => {
+  if (!newValue) {
+    menuComponent.value = "signup-menu"
+  } else {
+    menuComponent.value = "profile-menu"
+  }
+})
+</script>
