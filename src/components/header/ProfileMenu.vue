64fc99a639670623582917c49ea5df0dce64bab4
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-04-24 20:28:11 +0800

    Progress: Update Profile Menu

---
+++ src/components/header/ProfileMenu.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    class="header__signup col-8 col-sm-4 fw-semibold d-flex justify-content-evenly align-items-center text-decoration-none"
+    style="text-align: right"
+  >
+    <i class="fa-solid fa-user" style="color: #4c4ddc"></i>
+    <ul class="navbar-nav">
+      <li class="nav-item dropdown">
+        <a
+          class="nav-link dropdown-toggle"
+          href="#"
+          role="button"
+          data-bs-toggle="dropdown"
+        >
+          My Profile
+        </a>
+        <ul class="dropdown-menu">
+          <li class="dropdown-item">My Profile</li>
+          <li class="dropdown-item">Favorited Recipes</li>
+          <li class="dropdown-item">My Recipes</li>
+          <li><hr class="dropdown-divider" /></li>
+          <li class="dropdown-item" @click="logout">Logout</li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+
+const store = useStore()
+const router = useRouter()
+
+const logout = () => {
+  store.commit('auth/setUserLogout')
+  router.push("/")
+}
+</script>
+
+<style scope>
+    
+</style>
