c1f4ba4ac53485451bba589879366df35527ea02
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-04-24 20:28:47 +0800

    Progress: Update Index Router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../components/pages/HomePage.vue'
 import LoginPage from '../components/pages/LoginPage.vue'
 import SignupPage from '../components/pages/SignupPage.vue'
+import DetailPage from '../components/pages/DetailPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,7 @@ const router = createRouter({
     { path: '/', name: 'home', component: HomePage },
     { path: '/signup', name: 'signup', component: SignupPage },
     { path: '/login', name: 'login', component: LoginPage },
+    { path: '/recipe/:id', name: 'detailPage', component: DetailPage },
   ]
 })
 
