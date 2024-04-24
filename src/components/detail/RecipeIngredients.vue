3d98f70906034335d0f1093f4b09e6ec1d59d521
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-04-24 20:27:47 +0800

    Progress: Update Recipe Ingredients

---
+++ src/components/detail/RecipeIngredients.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="my-3">
+    <div class="card">
+        <div class="card-header">
+            <p class="fs-4 my-0 fw-semibold">Ingredients</p>
+        </div>
+        <div class="card-body">
+            <div class="mb-0">
+                <li v-for="(ingredients, index) in recipeDetail.ingredients" :key="index">{{ ingredients }}</li>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore()
+
+const recipeDetail = computed(() => {
+  return store.state.recipe.recipeDetail
+})
+</script>
+
+<style>
+
+</style>
