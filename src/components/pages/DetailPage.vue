ff515fb23d0f95f4e9096f61fb594f1ecaf31e76
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-04-24 20:28:26 +0800

    Progress: Update Detail Page

---
+++ src/components/pages/DetailPage.vue
@@ -0,0 +1,19 @@
+<script setup>
+import RecipeDetail from '@/components/detail/RecipeDetail.vue'
+</script>
+
+<template>
+  <main>
+    <div class="container-md my-5 py-5">
+        <div class="row my-4">
+            <div class="col-12 col-sm-2"></div>
+            <RecipeDetail />
+            <div class="col-12 col-sm-2"></div>
+        </div>
+    </div>
+  </main>
+</template>
+
+<style>
+
+</style>
